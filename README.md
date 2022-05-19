bc808c4519dd92c24cc3f96330583b4520376975
heralden <regen@anche.no>
2022-05-19 12:46:13 +0100

    Add :toc argument for different layout

--- README.md
+++ README.md
@@ -1,24 +1,35 @@
 # Følg
 
-> Stupidly simplistic self-hosted short-blogging platform.
+> Stupidly simplistic self-hosted blogging platform designed for offline composition
 
-Place markdown and image files into `resources/public` under a directory with a name to be ordered (usually YYYY-MM-DD). Each directory represents a single blog post with one markdown file optionally followed by one or more images. Blog posts are concatenated into a single *index.html* and sorted in reverse, so that the chronologically most recent post is at the top. Images within a blog post are sorted with the oldest first.
+The goal is to accommodate offline composition and publishing posts from any computer (e.g. smartphone using a file explorer app), which you'll use to create a directory with compressed images and a markdown file, then transfer it to your server once you're online. This program is only a static site generator - it's up to you to create the directories and files according to the spec, and provide a method for uploading to your server.
+
+## Client usage
+
+A markdown file needs to have a title, date and body. See [markdown-clj](https://github.com/yogthos/markdown-clj#supported-syntax) for supported syntax.
+
+```
+Title: Hello There!
+Date: 2022-05-01
+
+This is the first post.
+```
+
+Place markdown and image files into `resources/public` under a directory with a URL-friendly name. Each directory represents a single blog post with one markdown file optionally followed by one or more images. Blog posts are sorted in reverse, so that the chronologically most recent post is at the top. Images within a blog post are appended after the markdown text, and are sorted with increasing filenames.
 
 ```
 ▾ resources/public/
-  ▾ 2022-04-30/
+  ▾ hello/
       post.md
-  ▾ 2022-05-01/
+  ▾ bye/
       post.md
       P1050287.JPG
       P1050291.JPG
 ```
 
-Beyond ordering, directory and file names have no relevance.
+Beyond ordering of images, filenames have no relevance.
 
-The intention is to accommodate publishing posts from a smartphone using a file explorer app, which you'll use to create a directory with compressed images (to save mobile data) and a markdown file, then transfer it to your server.
-
-## Usage
+## Server usage
 
 Generate static files for a blog to the specified directory (will delete any contents).
 
@@ -31,6 +42,8 @@ Build initially, then rebuild on changes to resources.
 ```
 clojure -X folg/watch :title '"My Blog"' :out '"/var/www/blog"'
 ```
+
+By default, you'll get a *stream of thought* layout, which is suitable for many short posts with little text and few images. If you write fewer longer posts, you would probably prefer having a *table of contents* with each post being their own page. You can enable this with the `:toc true` argument.
 
 ## Customisation
 
--- src/folg.clj
+++ src/folg.clj
@@ -38,10 +38,34 @@ img {
   width: 100%;
   height: auto;
 }
+
+.post-date {
+  font-size: 0.5em;
+  opacity: 0.4;
+}
+
+.post-navigation {
+  display: flex;
+  justify-content: space-between;
+}
+
+.post-navigation > * {
+  flex: 1;
+  text-align: center;
+}
+
+.post-navigation > *:first-child {
+  text-align: left;
+}
+
+.post-navigation > *:last-child {
+  text-align: right;
+}
          </style>
          <title>" title "</title>
        </head>
-       <body>"
+       <body>
+         <h1>" title "</h1><hr>"
        s
        "</body>
        </html>"))
@@ -73,24 +97,88 @@ img {
   (= (get-related "/foo/bar.md" [{:path "/foo/baz.jpg"} {:path "/bar/bog.jpg"}])
      ["/foo/baz.jpg"]))
 
-(defn merge-pages [pages title]
-  {"/index.html"
-   (->> (sort-by key #(compare %2 %1) pages)
-        (map val)
-        (str/join "<hr>")
-        (wrap-html title))})
+(defn path->url
+  "Remove everything after last slash, making it trailing."
+  [filepath]
+  (str/replace filepath #"/[^/]*$" "/"))
+
+(defn create-toc [posts]
+  (str "<ul>"
+       (str/join
+         (map (fn [[path {:keys [metadata]}]]
+                (str "<a href=\"" (path->url path) "\"><li>" (first (:title metadata)) "</li></a>"))
+              posts))
+       "</ul>"))
+
+(def get-meta-title (comp first :title))
+(def get-meta-date (comp first :date))
+
+(defn post->html [[_ {:keys [metadata html]}]]
+  (str "<h2>" (get-meta-title metadata)
+       "<span class=\"post-date\"><br>" (get-meta-date metadata) "</span>"
+       "</h2>"
+       html))
+
+(defn wrap-navigation [{:keys [include-home no-top]}
+                       [prev-path {prev-meta :metadata}] [next-path {next-meta :metadata}] s]
+  (let [nav (str "<div class=\"post-navigation\">"
+                 "<span>"
+                 (when prev-path
+                   (str "« <a href=\"" (path->url prev-path) "\">" (get-meta-title prev-meta) "</a>"))
+                 "</span>"
+                 (when include-home
+                   "<a href=\"../\">Home</a>")
+                 "<span>"
+                 (when next-path
+                   (str "<a href=\"" (path->url next-path) "\">" (get-meta-title next-meta) "</a> »"))
+                 "</span>"
+                 "</div>")]
+    (str (when-not no-top nav) s nav)))
+
+(defn create-pages [pages title & {:keys [toc]}]
+  (let [posts (sort-by (comp first :date :metadata val) #(compare %2 %1) pages)]
+    (merge
+      (when toc
+        (into {} (map (fn [[prev-post [path :as post] next-post]]
+                        [(path->url path)
+                         (->> (post->html post)
+                              (wrap-navigation {:include-home true} prev-post next-post)
+                              (wrap-html title))]))
+              (partition 3 1 (concat [nil] posts [nil]))))
+      {"/index.html"
+       (wrap-html title (if toc
+                          (str (create-toc posts)
+                               (->> (post->html (first posts))
+                                    (wrap-navigation {:no-top true} nil (second posts))))
+                          (str/join "<hr>" (map post->html posts))))})))
 
 (comment
-  (= (merge-pages {"/1/path.html" "<p>text</p>"
-                   "/0/path.html" "<p>more text</p>"}
-                  "My title")
-     {"/index.html" (wrap-html "My title" "<p>text</p><hr><p>more text</p>")}))
+  (create-pages {"/foo/path.html" {:metadata {:title ["Foo"]
+                                              :date ["1990-01-01"]}
+                                   :html "<p>text</p>"}
+                 "/bar/path.html" {:metadata {:title ["Bar"]
+                                              :date ["1990-01-02"]}
+                                   :html "<p>more text</p>"}}
+               "My title")
+  {"/index.html" \space}
+
+  (create-pages {"/foo/path.html" {:metadata {:title ["Foo"]
+                                              :date ["1990-01-01"]}
+                                   :html "<p>text</p>"}
+                 "/bar/path.html" {:metadata {:title ["Bar"]
+                                              :date ["1990-01-02"]}
+                                   :html "<p>more text</p>"}}
+               "My title"
+               :toc true)
+  {"./foo/" \space
+   "./bar/" \space
+   "/index.html" \space})
 
 (defn get-file-names! []
   (keep #(when (.isFile %) (.getName %))
         (file-seq (io/file "resources/public"))))
 
-(defn generate-site! [{:keys [out title] :as _opts}]
+(defn generate-site! [{:keys [out title toc] :as _opts}]
   (when (.exists (io/file "resources/public"))
     (let [target-dir (str out)
           title (str (or title "Blog"))
@@ -103,11 +191,11 @@ img {
           pages (into {} (map (fn [[path md]]
                                 [(str/replace path #"\.md$" ".html")
                                  (-> (append-images md (get-related path assets-data))
-                                     (md/md-to-html-string))]))
+                                     (md/md-to-html-string-with-meta))]))
                       md-pages)]
       (stasis/empty-directory! target-dir)
       (optimus.export/save-assets assets target-dir)
-      (stasis/export-pages (merge-pages pages title) target-dir {:optimus-assets assets}))))
+      (stasis/export-pages (create-pages pages title :toc toc) target-dir {:optimus-assets assets}))))
 
 (defn validate-opts! [opts]
   (when (str/blank? (:out opts))
